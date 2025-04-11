af2f6d40e5294d8395da0821b754f50ba6397ca5
davi santos <azdavileandro@gmail.com>
2025-04-11 15:32:09 -0400

    calculadora de partidas rankeadas

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-let soma = 2 + 2
-console.log (soma)
-console.log("sou davi santos")
+let resultado = StatusPartida (200,21)
+let heroi = "Davi Santos"
+let nivel = ""
+
+function StatusPartida (vitorias,derrotas){
+  let quantidade = vitorias - derrotas
+  return quantidade
+}
+  
+  switch (true){
+      case resultado < 10:
+        nivel = "Ferro"
+        break
+        case resultado < 21:
+        nivel = "Bronze"
+        break
+        case resultado < 51:
+        nivel = "Prata"
+        break
+        case resultado < 81:
+        nivel = "Ouro"
+        break
+        case resultado < 91:
+        nivel = "Diamante"
+        break
+        case resultado < 101:
+        nivel = "Lendário"
+        break
+        default :
+        nivel = "Imortal"
+
+}
+
+console.log ("O heroi " + heroi + " tem saldo de " + resultado + " vitorias e está no nivel " + nivel)
